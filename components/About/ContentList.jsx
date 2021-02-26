4497c32f6a7476395e8286662809cf1476e3c0f1
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-02-26 23:07:20 +0200

    fix image

--- components/About/ContentList.jsx
+++ components/About/ContentList.jsx
@@ -2,8 +2,6 @@ import { RichText } from 'prismic-reactjs';
 import cn from 'classnames';
 import Image from 'next/image';
 
-import styles from './ContentList.module.css';
-
 const ContentList = ({ content }) => {
   if (!content) {
     return null;
@@ -18,14 +16,16 @@ const ContentList = ({ content }) => {
     const sectionClass = cn('about-us', { 'section-bg': !even });
     const image = (
       <div className="about-us-item">
-        {/* <Img loading="auto" fluid={item.item_imageSharp.childImageSharp.fluid} /> */}
-        <div style={{ position: 'relative', height: '100%', width: '100%' }}>
-          {/* FIXME: image must be relative or need change layout */}
-          <Image src={imageUrl} layout="fill" objectFit="cover" className={styles.jopa} />
-        </div>
+        <Image
+          src={imageUrl}
+          layout="responsive"
+          objectFit="cover"
+          width="1600"
+          height="1200"
+        />
       </div>
     );
-    const content = RichText.render(item.content);
+    const text = RichText.render(item.text);
 
     return (
       <section key={key} className={sectionClass}>
@@ -33,10 +33,10 @@ const ContentList = ({ content }) => {
 
           <div className="row content">
             <div className="col-lg-6" data-aos="fade-right">
-              {even ? content : image}
+              {even ? text : image}
             </div>
             <div className="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
-              {even ? image : content}
+              {even ? image : text}
             </div>
           </div>
 
