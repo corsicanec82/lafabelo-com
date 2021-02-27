1f0f361b4a834135820432d70cbeffd72b905179
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-02-27 13:57:52 +0200

    fix load snipcart

--- components/Header/Cart.jsx
+++ components/Header/Cart.jsx
@@ -1,19 +1,10 @@
-import Head from 'next/head';
-
-const API_KEY = process.env.NEXT_PUBLIC_SNIPCART_API_KEY;
-
 const Cart = () => (
   <>
-    <Head>
-      <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.29/default/snipcart.css" />
-      <script async src="https://cdn.snipcart.com/themes/v3.0.29/default/snipcart.js" />
-    </Head>
     <div className="header-cart">
       <button type="button" className="btn btn-link icofont-cart snipcart-checkout">
         <span className="snipcart-total-price" />
       </button>
     </div>
-    <div id="snipcart" data-api-key={API_KEY} hidden />
   </>
 );
 
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/style.css';
 import '../styles/icofont/icofont.min.css';
 import '../styles/google/opensans.css';
 import '../styles/google/roboto.css';
@@ -7,23 +6,32 @@ import '../styles/google/poppins.css';
 import '../styles/remixicon/remixicon.css';
 import '../styles/boxicons/css/boxicons.css';
 import 'aos/dist/aos.css';
+import '../styles/style.css';
 
 import { useEffect } from 'react';
 import aos from 'aos';
+import Head from 'next/head';
 
 import Container from '../components/Container.jsx';
 import BackToTopButton from '../components/BackToTopButton.jsx';
+import loadSnipcart from '../lib/snipcart.js';
 
 const App = ({ Component, pageProps }) => {
   useEffect(() => {
     aos.init({ duration: 700 });
+    loadSnipcart();
   }, []);
 
   return (
-    <Container>
-      <Component {...pageProps} />
-      <BackToTopButton />
-    </Container>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/snipcart.css" />
+      </Head>
+      <Container>
+        <Component {...pageProps} />
+        <BackToTopButton />
+      </Container>
+    </>
   );
 };
 
