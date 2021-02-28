dc16258035a673ed070a32d569b5ecd8eedac796
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-03-01 00:07:40 +0200

    fix locale detect: try use domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ module.exports = {
   i18n: {
     locales: ['ru', 'ua', 'de'],
     defaultLocale: 'de',
+    domains: [
+      {
+        domain: 'lafabelo.ru',
+        defaultLocale: 'de',
+        locales: ['ru', 'ua', 'de'],
+      },
+    ],
   },
   images: {
     domains: [
