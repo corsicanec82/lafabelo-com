15d52eb34e2fc775db1fe4486c0c8e6243f7f2c2
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-03-01 09:16:00 +0200

    fix next config with locales

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
+// TODO: remove after implements detect language
+const productionDomains = {
+  domains: [
+    {
+      domain: 'lafabelo.ru',
+      defaultLocale: 'de',
+      locales: ['ru', 'ua', 'de'],
+    },
+  ],
+};
+
+const domains = process.env.NODE_ENV === 'production'
+  ? productionDomains
+  : {};
+
 module.exports = {
   i18n: {
     locales: ['ru', 'ua', 'de'],
     defaultLocale: 'de',
-    domains: [
-      {
-        domain: 'lafabelo.ru',
-        defaultLocale: 'de',
-        locales: ['ru', 'ua', 'de'],
-      },
-    ],
+    ...domains,
+    localeDetection: false,
   },
   images: {
     domains: [
