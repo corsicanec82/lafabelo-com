806bfedb46f70a77b8f468b3456cc52935eec251
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-03-08 17:36:33 +0200

    remove domain from config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,7 @@
-// TODO: remove after implements detect language
-const productionDomains = {
-  domains: [
-    {
-      domain: 'lafabelo.ru',
-      defaultLocale: 'de',
-      locales: ['de'],
-    },
-    {
-      domain: 'lafabelo.ru/ru',
-      defaultLocale: 'ru',
-      locales: ['ru'],
-    },
-    {
-      domain: 'lafabelo.ru/ua',
-      defaultLocale: 'ua',
-      locales: ['ua'],
-    },
-  ],
-};
-
-const domains = process.env.NODE_ENV === 'production'
-  ? productionDomains
-  : {};
-
 module.exports = {
   i18n: {
     locales: ['ru', 'ua', 'de'],
     defaultLocale: 'de',
-    ...domains,
-    // localeDetection: false,
   },
   images: {
     domains: [
