7d9ad189d9f3bbc9a40b0987fee6f31d7e6adc56
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-03-08 17:26:36 +0200

    try detect language with domains

--- next.config.js
+++ next.config.js
@@ -1,23 +1,33 @@
 // TODO: remove after implements detect language
-// const productionDomains = {
-//   domains: [
-//     {
-//       domain: 'lafabelo.ru',
-//       defaultLocale: 'de',
-//       locales: ['ru', 'ua', 'de'],
-//     },
-//   ],
-// };
+const productionDomains = {
+  domains: [
+    {
+      domain: 'lafabelo.ru',
+      defaultLocale: 'de',
+      locales: ['de'],
+    },
+    {
+      domain: 'lafabelo.ru/ru',
+      defaultLocale: 'ru',
+      locales: ['ru'],
+    },
+    {
+      domain: 'lafabelo.ru/ua',
+      defaultLocale: 'ua',
+      locales: ['ua'],
+    },
+  ],
+};
 
-// const domains = process.env.NODE_ENV === 'production'
-//   ? productionDomains
-//   : {};
+const domains = process.env.NODE_ENV === 'production'
+  ? productionDomains
+  : {};
 
 module.exports = {
   i18n: {
     locales: ['ru', 'ua', 'de'],
     defaultLocale: 'de',
-    // ...domains,
+    ...domains,
     // localeDetection: false,
   },
   images: {
